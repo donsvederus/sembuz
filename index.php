175365dca87f0e7a2a2dcb0d0fc68e02796148fc
donsvederus <svederus@gmail.com>
2022-04-30 01:17:34 -0600

    0.008 Created Style Sheet

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="shortcut icon" type="image" href="images/favicon.png"/>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
     <div class="mainSection">
