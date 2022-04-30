c3f9e9f2cdf93d93cc849a5079ed24b4a7b321da
donsvederus <svederus@gmail.com>
2022-04-30 16:36:57 -0600

    0.026 Relative Links to Absolute, and Recursively crawling links

--- crawl.php
+++ crawl.php
@@ -1,11 +1,33 @@
 <?php
 include("classes/DomDocumentParser.php");
 
+$alreadyCrawled = array();
+$crawling = array();
+
 function createLink($src, $url) {
 
+    $scheme = parse_url($url)["scheme"]; // http
+    $host = parse_url($url)["host"]; // www.svederus.com
+
+    if(substr($src, 0, 2) == "//") {
+        $src = $scheme + ":" . $src;
+    } else if(substr($src, 0, 1) == "/") {
+        $src = $scheme . "://" . $host . $src;
+    } else if(substr($src, 0, 2) == "./") {
+        $src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src, 1);
+    } else if(substr($src, 0, 3) == "../") {
+        $src = $scheme . "://" . $host . "/" . $src;
+    } else if(substr($src, 0, 5) != "https" && substr($src, 0, 4) != "http") {
+        $src = $scheme . "://" . $host . "/" . $src;
+    } 
+
+    return $src;
 }
 
 function followLinks($url) {
+
+    global $alreadyCrawled;
+    global $crawling;
     
     $parser = new DomDocumentParser($url);
 
@@ -20,9 +42,22 @@ function followLinks($url) {
             continue;
         }
 
-        createLink($href, $url);
+        $href = createLink($href, $url);
 
-        //echo $href . "<br>";
+        if(!in_array($href, $alreadyCrawled)) {
+            $alreadyCrawled[] = $href;
+            $crawling[] = $href;
+
+            // insert $href
+        }
+
+        echo $href . "<br>";
+    }
+
+    array_shift($crawling);
+
+    foreach($crawling as $site) {
+        followLinks($site);
     }
 
 }
