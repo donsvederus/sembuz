86f07fc008b71b9a3a2a803dec1ed898e02cc3e7
donsvederus <svederus@gmail.com>
2022-04-30 00:57:37 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="shortcut icon" type="image" href="images/favicon.png"/>
+</head>
+<body>
+
+    <div class="mainSection">
+
+        <div class="logoContainer">
+            <img src="images/sembuz-logo-800x500.jpg" alt="" width="400">
+        </div>
+
+        <div class="searchContainer">
+            <input type="text" class="searchBox" name="term">
+            <input type="submit" class="searchButton" value="Search">
+        </div>
+
+    </div>
+    
+</body>
+</html>
