0ad15c81db3f8be98dc8b64afc0b748b4c523c50
Don Svederus <svederus@gmail.com>
2022-05-30 12:37:45 -0600

    0.039 Trimming the result text

--- classes/SiteResultsProvider.php
+++ classes/SiteResultsProvider.php
@@ -44,6 +44,9 @@ class SiteResultsProvider {
             $title = $row["title"];
             $description = $row["description"];
 
+            $title = $this->trimField($title, 55);
+            $description = $this->trimField($description, 230);
+
             $resultsHtml .= "<div class='resultContainer'>
             <h3 class='title'>
                 <a class='result' href='$url'>$title</a>
@@ -56,6 +59,13 @@ class SiteResultsProvider {
         $resultsHtml .= "</div>";
 
         return $resultsHtml;
+
+    }
+
+    private function trimField($string, $characterLimit) {
+
+        $dots = strlen($string) > $characterLimit ? "..." : "";
+        return substr($string, 0, $characterLimit) . $dots;
 
     }
 
